8d0c15a49ba2b1f72cc466c81596b6dedcd2b78e
Bilasioo <bilasiobilly@gmial.com>
2024-05-22 00:12:16 +0300

    User numerical input

---
+++ accepting_numerical_inputs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+	int a, b, ans;
+
+	printf("Input your first value:  ");
+	scanf("%d", &a);
+
+	printf("Input your second value:  ");
+        scanf("%d", &b);
+
+	ans = a + b;
+
+	printf("Your answer is: %d\n", ans);
+
+}
